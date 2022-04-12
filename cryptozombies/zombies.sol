e7ab021a35dbef4de73fc29d9822a28f7c95a26d
Nicholas Tillmann <nit36@pitt.edu>
2022-04-12 17:05:56 -0400

    adding mappings to solidity

--- cryptozombies/zombies.sol
+++ cryptozombies/zombies.sol
@@ -14,8 +14,12 @@ contract ZombieFactory {
 
     Zombie[] public zombies;
 
+    mapping (uint => address) public zombieToOwner;
+    mapping (address => uint) ownerZombieCount;
+
     function _createZombie(string memory _name, uint _dna) private {
         uint id = zombies.push(Zombie(_name, _dna)) - 1;
+        // start here
         emit NewZombie(id, _name, _dna);
     }
 
